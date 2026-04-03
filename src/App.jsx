904e86e6ee8a950e25cad414bdee32484f80c45b
DemonSinger <30102823@student.toiohomai.ac.nz>
2026-04-03 14:51:39 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router'
+import {BrowserRouter, Routes, Route} from 'react-router'
 import NavMenu from './NavMenu'
 import ScpDetail from './ScpDetail'
 import Home from './Home'
 
 export default function App()
-
 {
   return (
     <>
@@ -12,13 +11,13 @@ export default function App()
         <h1>SCP Foundation</h1>
         <p>Secure, Contain, Protect</p>
       </header>
-      <Router>
+      <BrowserRouter basename="/SCP-Vite-2026">
         <NavMenu />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/scp/:item' element={<ScpDetail />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
       <footer>
         <p>2026 SCP Foundation | All rights reserved.</p>
       </footer>
